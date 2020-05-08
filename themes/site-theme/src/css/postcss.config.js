7b1755278a820da5e88c7bba35cf2a53e90509b9
Brendon Folsom <admin@greenzero.us>
2020-05-08 14:27:40 -0400

    swap global css partials to txt format for compatability with purgecss

--- themes/site-theme/src/css/postcss.config.js
+++ themes/site-theme/src/css/postcss.config.js
@@ -9,7 +9,7 @@ module.exports = {
     'tailwindcss': ('./css-config.js'),
     'cssnano': {},
     '@fullhuman/postcss-purgecss': {
-      content: ['../../../layouts/**/*.html', '../layouts/**/*.html','../static/**/*.js'],
+      content: ['../../../layouts/**/*.html', '../layouts/**/*.html', '../layouts/**/*.txt', '../static/**/*.js'],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     },
 	}
